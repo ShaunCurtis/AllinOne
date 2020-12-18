a0b64d559ef01dc91b61e5b1adb02d4c1622b1bb
Shaun Curtis <shaun@coldelm.com>
2020-12-18 14:11:22 +0000

    Update to Net5 Add New Counter Page

--- All-In-One/Client/Program.cs
+++ All-In-One/Client/Program.cs
@@ -16,10 +16,11 @@ namespace AllinOne.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add < AllinOne.Shared.Components.WASM>("app");
+            builder.RootComponents.Add < AllinOne.Shared.Components.WASM>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IWeatherForecastService, WeatherForecastWASMService>();
+            builder.Services.AddSingleton<CounterService>();
 
             await builder.Build().RunAsync();
         }
